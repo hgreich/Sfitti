6b762cb44747dd26ac2527e3dc1a529bb9e4cd21
Hannah G Reich <44375907+hgreich@users.noreply.github.com>
2020-06-16 16:35:09 -0400

    Add files via upload

---
+++ genome_analyses/blastdbacropora.sh
@@ -0,0 +1,18 @@
+#PBS -l nodes=1:ppn=1
+
+#PBS -l walltime=24:00:00 #Time to run, our max limit is 24hrs
+
+#PBS -l pmem=64gb
+
+#PBS -j oe
+
+
+cd /gpfs/home/hgr5/raw
+
+module load blast/2.2.26 
+module load blast+/2.2.26 
+
+makeblastdb -in ~/raw/digitifera.fa -dbtype nucl -out ~/raw/digitifera -parse_seqids
+
+echo " "
+echo "Job Ended at `date`"
---
+++ genome_analyses/split_blast_symb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+exec < "list"
+while read LINE
+do
+
+	FN=$LINE
+	blastn -task megablast -query $FN -db ~/scratch/raw/symbiodinium -evalue 1e-5 -num_threads 1  -max_target_seqs 1 -outfmt 6 -out $LINE.sym
+
+done
+
